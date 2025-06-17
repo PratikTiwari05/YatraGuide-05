909505fbc9c8fd77ce44fb17fc97b77d3b108d99
pratik-tgit05 <pratik12122005@gmail.com>
2025-06-17 19:38:31 +0530

    fix: connect to Railway MySQL using env variables

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,       
-  user: process.env.DB_USER,       
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_NAME,   
-  port: process.env.DB_PORT        
-});
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+  })
+  .promise();
 
-module.exports = db;
+module.exports = pool;
