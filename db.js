e851417e4e6613f4556466b6acd753b3a5917c8c
pratik-tgit05 <pratik12122005@gmail.com>
2025-06-17 18:48:12 +0530

    updated db.js to use env variables and ignored .env

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
-const mysql=require('mysql2');
-    const pool=mysql.createPool({
-        host:'localhost',
-        user:'root',
-        password:'pass123',
-        database:'railway_db',
-        waitForConnections:true,
-        connectionLimit:10,
-        queueLimit:0
-    });
-    module.exports=pool.promise();
+// db.js
+const mysql = require('mysql2');
+require('dotenv').config(); 
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,         
+  user: process.env.DB_USER,         
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,     
+  port: process.env.DB_PORT,         
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool.promise();
